c9294cfdb76843d766cac04e003fa8ccd9631438
Peter Zotov <whitequark@whitequark.org>
2014-11-18 18:02:09 +0300

    Migrate to cstubs.

---
+++ lib_gen/sodium_bindings.ml
@@ -0,0 +1,165 @@
+open Ctypes
+
+module C(F: Cstubs.FOREIGN) = struct
+  let prefix = "sodium"
+
+  let init    = F.foreign (prefix^"_init")    (void @-> returning void)
+  let memzero = F.foreign (prefix^"_memzero") (ocaml_bytes @-> size_t @-> returning void)
+  let memcmp  = F.foreign (prefix^"_memcmp")  (ocaml_bytes @-> ocaml_bytes @-> size_t @-> returning void)
+
+  module Verify = struct
+    let verify_type = ocaml_bytes @-> ocaml_bytes @-> returning int
+    let verify_16   = F.foreign "crypto_verify_16" verify_type
+    let verify_32   = F.foreign "crypto_verify_32" verify_type
+    let verify_64   = F.foreign "crypto_verify_64" verify_type
+  end
+
+  module Random = struct
+    let stir = F.foreign "randombytes_stir" (void @-> returning void)
+
+    module Make(T: Sodium_storage.S) = struct
+      let gen  = F.foreign "randombytes_buf"  (T.ctype @-> size_t @-> returning void)
+    end
+  end
+
+  module Box = struct
+    let primitive = "curve25519xsalsa20poly1305"
+    let prefix    = "crypto_box_"^primitive
+
+    let sz_query_type    = void @-> returning size_t
+    let publickeybytes   = F.foreign (prefix^"_publickeybytes") sz_query_type
+    let secretkeybytes   = F.foreign (prefix^"_secretkeybytes") sz_query_type
+    let beforenmbytes    = F.foreign (prefix^"_beforenmbytes")  sz_query_type
+    let noncebytes       = F.foreign (prefix^"_noncebytes")     sz_query_type
+    let zerobytes        = F.foreign (prefix^"_zerobytes")      sz_query_type
+    let boxzerobytes     = F.foreign (prefix^"_boxzerobytes")   sz_query_type
+
+    let box_keypair      = F.foreign (prefix^"_keypair")
+                                     (ocaml_bytes @-> ocaml_bytes @-> returning int)
+
+    let box_beforenm     = F.foreign (prefix^"_beforenm")
+                                     (ocaml_bytes @-> ocaml_bytes @-> ocaml_bytes
+                                      @-> returning int)
+
+    module Make(T: Sodium_storage.S) = struct
+      let box_fn_type      = (T.ctype @-> T.ctype @-> ullong
+                              @-> ocaml_bytes @-> ocaml_bytes @-> ocaml_bytes
+                              @-> returning int)
+
+      let box              = F.foreign (prefix) box_fn_type
+      let box_open         = F.foreign (prefix^"_open") box_fn_type
+
+      let box_afternm_type = (T.ctype @-> T.ctype @-> ullong
+                              @-> ocaml_bytes @-> ocaml_bytes @-> returning int)
+
+      let box_afternm      = F.foreign (prefix^"_afternm") box_afternm_type
+      let box_open_afternm = F.foreign (prefix^"_open_afternm") box_afternm_type
+    end
+  end
+
+  module Sign = struct
+    let primitive = "ed25519"
+    let prefix    = "crypto_sign_"^primitive
+
+    let sz_query_type   = void @-> returning size_t
+    let publickeybytes  = F.foreign (prefix^"_publickeybytes") sz_query_type
+    let secretkeybytes  = F.foreign (prefix^"_secretkeybytes") sz_query_type
+    let bytes           = F.foreign (prefix^"_bytes")          sz_query_type
+
+    let sign_keypair    = F.foreign (prefix^"_keypair")
+                                    (ocaml_bytes @-> ocaml_bytes @-> returning int)
+
+    module Make(T: Sodium_storage.S) = struct
+      let sign_fn_type    = (T.ctype @-> ptr ullong @-> T.ctype
+                             @-> ullong @-> ocaml_bytes @-> returning int)
+
+      let sign            = F.foreign (prefix) sign_fn_type
+      let sign_open       = F.foreign (prefix^"_open") sign_fn_type
+    end
+  end
+
+  module Scalar_mult = struct
+    let primitive = "curve25519"
+    let prefix    = "crypto_scalarmult_"^primitive
+
+    let sz_query_type   = void @-> returning size_t
+    let bytes           = F.foreign (prefix^"_bytes") sz_query_type
+    let scalarbytes     = F.foreign (prefix^"_scalarbytes") sz_query_type
+
+    let scalarmult      = F.foreign (prefix)
+                                    (ocaml_bytes @-> ocaml_bytes @-> ocaml_bytes
+                                     @-> returning int)
+    let scalarmult_base = F.foreign (prefix^"_base")
+                                    (ocaml_bytes @-> ocaml_bytes @-> returning int)
+  end
+
+  module Secret_box = struct
+    let primitive = "xsalsa20poly1305"
+    let prefix    = "crypto_secretbox_"^primitive
+
+    let sz_query_type   = void @-> returning size_t
+    let keybytes        = F.foreign (prefix^"_keybytes")     sz_query_type
+    let noncebytes      = F.foreign (prefix^"_noncebytes")   sz_query_type
+    let zerobytes       = F.foreign (prefix^"_zerobytes")    sz_query_type
+    let boxzerobytes    = F.foreign (prefix^"_boxzerobytes") sz_query_type
+
+    module Make(T: Sodium_storage.S) = struct
+      let secretbox_fn_ty = (T.ctype @-> T.ctype @-> ullong
+                             @-> ocaml_bytes @-> ocaml_bytes @-> returning int)
+
+      let secretbox       = F.foreign (prefix)         secretbox_fn_ty
+      let secretbox_open  = F.foreign (prefix^"_open") secretbox_fn_ty
+    end
+  end
+
+  module Stream = struct
+    let primitive = "xsalsa20"
+    let prefix    = "crypto_stream_"^primitive
+
+    let sz_query_type   = void @-> returning size_t
+    let keybytes        = F.foreign (prefix^"_keybytes")     sz_query_type
+    let noncebytes      = F.foreign (prefix^"_noncebytes")   sz_query_type
+
+    module Make(T: Sodium_storage.S) = struct
+      let stream          = F.foreign (prefix)
+                                      (T.ctype @-> ullong @-> ocaml_bytes
+                                       @-> ocaml_bytes @-> returning int)
+      let stream_xor      = F.foreign (prefix^"_xor")
+                                      (T.ctype @-> T.ctype @-> ullong
+                                       @-> ocaml_bytes @-> ocaml_bytes @-> returning int)
+    end
+  end
+
+  module Gen_auth(M: sig
+    val scope     : string
+    val primitive : string
+  end) = struct
+    let primitive = M.primitive
+    let prefix    = "crypto_"^M.scope^"_"^primitive
+
+    let sz_query_type = void @-> returning size_t
+    let keybytes      = F.foreign (prefix^"_keybytes") sz_query_type
+    let bytes         = F.foreign (prefix^"_bytes")    sz_query_type
+
+    module Make(T: Sodium_storage.S) = struct
+      let auth_fn_type  = (ocaml_bytes @-> T.ctype @-> ullong
+                           @-> ocaml_bytes @-> returning int)
+
+      let auth          = F.foreign (prefix)           auth_fn_type
+      let auth_verify   = F.foreign (prefix^"_verify") auth_fn_type
+    end
+  end
+
+  module Hash = struct
+    let primitive = "sha512"
+    let prefix    = "crypto_hash_"^primitive
+
+    let sz_query_type = void @-> returning size_t
+    let hashbytes     = F.foreign (prefix^"_bytes") sz_query_type
+
+    module Make(T: Sodium_storage.S) = struct
+      let hash          = F.foreign (prefix)
+                                    (ocaml_bytes @-> T.ctype @-> ullong @-> returning int)
+    end
+  end
+end
